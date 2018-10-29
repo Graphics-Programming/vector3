119891eac4a739e7b7378c5542268971e2719f40
pavlosp1967 <pavlosp1967@gmail.com>
2018-10-29 13:59:10 -0700

    update 10 functions

--- math/vector3.js
+++ math/vector3.js
@@ -2,82 +2,100 @@
  * An object representing a 3d vector to make operations simple and concise.
  */
 
-var Vector3 = function(x, y, z) {
-	this.x = x; this.y = y; this.z = z;
-	
+var Vector3 = function (x = 0, y = 0, z = 0) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
 	// todo - make sure to set a default value in case x, y, or z is not passed in
 
-	this.set = function(x, y, z) {
+	this.set = function (x, y, z) {
 		//todo set this' values to those from x, y, and z
-		
+		this.x = x;
+		this.y = y;
+		this.z = z;
 		return this;
 	}
 
-	this.clone = function() {
+	this.clone = function () {
 		return new Vector3(this.x, this.y, this.z);
 	};
 
-	this.copy = function(other) {
+	this.copy = function (other) {
 		// copy the values from other into this
-		
+		this.x = other.x;
+		this.y = other.y;
+		this.z = other.z;
 		return this;
 	}
 
-	this.add = function(v) {
+	this.add = function (v) {
 		// todo - add v to this vector (1pt)
 		// This SHOULD change the values of this.x, this.y, and this.z
+		this.x = this.x + v.x;
+		this.y = this.y + v.y;
+		this.z = this.z + v.z;
 		return this;
 	};
 
-	this.subtract = function(v) {
+	this.subtract = function (v) {
 		// todo - subtract v from this vector (1pt)
 		// This SHOULD change the values of this.x, this.y, and this.z
+		this.x = this.x - v.x;
+		this.y = this.y - v.y;
+		this.z = this.z - v.z;
 		return this;
 	};
 
-	this.negate = function() {
+	this.negate = function () {
 		// multiply this vector by -1 (1pt)
 		// This SHOULD change the values of this.x, this.y, and this.z
+		this.x = this.x * -1;
+		this.y = this.y * -1;
+		this.z = this.z * -1;
 		return this;
 	};
 
-	this.multiplyScalar = function(scalar) {
+	this.multiplyScalar = function (scalar) {
 		// multiply this vector by "scalar" (1pt)
 		// This SHOULD change the values of this.x, this.y, and this.z
+		this.x = this.x * scalar;
+		this.y = this.y * scalar;
+		this.z = this.z * scalar;
 		return this;
 	};
 
-	this.length = function() {
+	this.length = function () {
 		// todo - return the magnitude (a.k.a length) of this vector (1pt)
 		// This should NOT change the values of this.x, this.y, and this.z
-		return 0;
+		return (Math.sqrt((this.x * this.x) + (this.y * this.y)+this.z *this.z));
 	};
 
-	this.lengthSqr = function() {
+	this.lengthSqr = function () {
 		// todo - return the squared magnitude of this vector ||v||^2 (1pt)
 		// This should NOT change the values of this.x, this.y, and this.z
-		return 0;
+		return (this.x * this.x) + (this.y * this.y)+(this.z *this.z);
 	};
 
-	this.normalized = function() {
+	this.normalized = function () {
 		// todo - return a new vector that is a normalized version of this vector (1pt)
 		// This should NOT change the values of this.x, this.y, and this.z
-		return null; // Should return a new vector that is not this
+		 return this;// Should return a new vector that is not this
 	};
 
-	this.normalize = function() {
+	this.normalize = function () {
 		// todo - Change the components of this vector so that its magnitude will equal 1. (1pt)
 		// This SHOULD change the values of this.x, this.y, and this.z
-		return this;
+		
+		return this; 
 	};
 
-	this.dot = function(other) {
+	this.dot = function (other) {
 		// todo - return the dot product between this vector and "other" (5pt)
 		// This should NOT change the values of this.x, this.y, and this.z
 		return 0;
 	};
 
-	this.cross = function(other) {
+	this.cross = function (other) {
 		// todo - return the cross product (as a new vector) between this vector and "other" (3pt)
 		// This should NOT change the values of this.x, this.y, and this.z
 		return null;
